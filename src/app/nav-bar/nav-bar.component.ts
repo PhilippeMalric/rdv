6d90fae8a1ec30d851a4135df68a6f87699b3276
Philippe <malric.philippe@gmail.com>
2018-06-27 11:31:09 -0400

    animation added

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,8 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
+  animations: [
+    trigger('panelState', [
+      state('hidden', style({
+        opacity: 0
+      })),
+      state('visible', style({
+        opacity: 1
+      })),
+      transition('visible => hidden', animate('300ms ease-in')),
+      transition('hidden => visible', animate('300ms ease-out'))
+    ])
+  ],
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent implements OnInit {
