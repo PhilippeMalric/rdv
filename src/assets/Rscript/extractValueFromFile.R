6dc62b25cc34b35081bab75720620545c6a986a9
Unknown <malric.philippe@gmail.com>
2018-08-03 03:47:43 -0400

    Rscript

---
+++ src/assets/Rscript/extractValueFromFile.R
@@ -0,0 +1,26 @@
+install.packages("stringr")
+library("stringr")
+library("rjson")
+
+# pred analysis
+
+folder = "C:/Users/p/Desktop/prediction/"
+files = list.files(path = folder,recursive = TRUE)
+f = files[[1]]
+
+str_match(files, "S_(.*)_stn_(.*)_ed_(.*)/")
+
+readFile = function(f)
+{
+  info = str_match(f, "S_(.*)_stn_(.*)_ed_(.*)/")
+  y = fromJSON(file = paste(folder,f,sep=""))
+  y["fileName"] = f
+  y["s"] = info[2]
+  y["stn"] = info[3]
+  y["ed"] = info[4]
+  
+}
+
+fileList = lapply(files,readFile)
+
+
